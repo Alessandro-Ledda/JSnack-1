714270416231c9f8ff934e9974ad541ace8ad061
Alessandro-Ledda <aleledda75@icloud.com>
2025-12-04 11:30:32 +0100

    esercizio speed in classe

---
+++ script.js
@@ -0,0 +1,20 @@
+/*CONSEGNA
+ JSnack 1
+ L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+*/
+
+//OUTPUT 
+//CHIEDO ALL'UTENTE IL PRIMO NUMERO
+//CONTROLLO I DUE NUMERI SCELTI ASSOCIANDOLI A DUE VARIABILI
+const primoNum = parseInt(prompt("Dammi il primo numero"));
+const secondoNum = parseInt(prompt("dammi il secondo numero"));
+//CHIEDO ALL'UTENTE IL SECONDO NUMERO
+
+//ELABORAZIONE
+if (primoNum > secondoNum){
+    console.log("il numero maggiore è" + " " + primoNum);
+}else{
+    console.log("Il numero più grande è" + " " + secondoNum);
+}
+
+//SE IL PRIMO NUMERO E' PIU' GRANDE STAMPO  IL PRIMO NUMERO ALTRIMENTI STAMPO IL SECONDO
